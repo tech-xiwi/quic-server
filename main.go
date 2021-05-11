763f679c2ce863a626e122ead589a4e04d14fe29
xiwi <xiwi@hellotalk.com>
2021-05-11 23:21:17 +0800

    init

---
+++ main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"io"
+	"log"
+	"math/big"
+
+	quic "github.com/lucas-clemente/quic-go"
+)
+
+const addr = ":4242"
+
+const message = "foobar"
+
+// We start a server echoing data on the first stream the client opens,
+// then connect with a client, send the message, and wait for its receipt.
+func main() {
+	go func() { log.Fatal(echoServer()) }()
+
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			err := clientMain(i)
+			if err != nil {
+				panic(err)
+			}
+		}(i)
+	}
+	select {}
+}
+
+// Start a server that echos all data on the first stream opened by the client
+func echoServer() error {
+	listener, err := quic.ListenAddr(addr, generateTLSConfig(), nil)
+	if err != nil {
+		return err
+	}
+	for {
+		sess, err := listener.Accept(context.Background())
+		if err != nil {
+			return err
+		}
+		go func(sess quic.Session) {
+			stream, err := sess.AcceptStream(context.Background())
+			if err != nil {
+				panic(err)
+			}
+			// Echo through the loggingWriter
+			_, err = io.Copy(loggingWriter{stream}, stream)
+			if err != nil {
+				panic(err)
+			}
+		}(sess)
+	}
+}
+
+func clientMain(i int) error {
+	tlsConf := &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"quic-echo"},
+	}
+	session, err := quic.DialAddr(addr, tlsConf, nil)
+	if err != nil {
+		return err
+	}
+
+	stream, err := session.OpenStreamSync(context.Background())
+	if err != nil {
+		return err
+	}
+	defer stream.Close()
+
+	msg := fmt.Sprintf("%s:%d", message, i)
+
+	fmt.Printf("Client: Sending '%s'\n", msg)
+	_, err = stream.Write([]byte(msg))
+	if err != nil {
+		return err
+	}
+
+	buf := make([]byte, len(msg))
+	_, err = io.ReadFull(stream, buf)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Client: Got '%s'\n", buf)
+
+	return nil
+}
+
+// A wrapper for io.Writer that also logs the message.
+type loggingWriter struct{ io.Writer }
+
+func (w loggingWriter) Write(b []byte) (int, error) {
+	fmt.Printf("Server: Got '%s'\n", string(b))
+	return w.Writer.Write(b)
+}
+
+// Setup a bare-bones TLS config for the server
+func generateTLSConfig() *tls.Config {
+	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		panic(err)
+	}
+	template := x509.Certificate{SerialNumber: big.NewInt(1)}
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	if err != nil {
+		panic(err)
+	}
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+
+	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		panic(err)
+	}
+	return &tls.Config{
+		Certificates: []tls.Certificate{tlsCert},
+		NextProtos:   []string{"quic-echo"},
+	}
+}
